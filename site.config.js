be44757baef47d2fac88445d6e962ac645292c24
DVDphobia <l1gend@outlook.com>
2024-02-24 13:27:13 +0000

    t

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "dvdphobia",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    bio: "I develop everything using C/C++.",
+    email: "dvdphobia@gmail.com",
+    linkedin: "",
+    github: "dvdphobia",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `Dvdphiba`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
     title: "morethan-log",
-    description: "welcome to morethan-log!",
+    description: "welcome to Dvdphobia!",
   },
 
   // CONFIG configration (required)
